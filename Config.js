893a099ac87d6a20c1d6a19c98bfa050a03d2f85
acrey1-haule <hauleacrey1@gmail.com>
2025-02-20 10:37:11 +0300

    Create Config.js

---
+++ Config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  botName: "REY-BOT",
+  ownerNumber: "123456789@s.whatsapp.net",  // Replace with your number
+  alwaysOnline: true,
+  autoReactStatus: true,
+  antiViewOnce: true,
+  antiDelete: true
+};
